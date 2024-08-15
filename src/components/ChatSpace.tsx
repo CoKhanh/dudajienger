1a0e4f2188134510487b1e2391133857cef7ed15
CoKhanh <duongcokhanh17110315@gmail.com>
2024-08-15 10:13:07 +0700

    Add message UI

--- src/components/ChatSpace.tsx
+++ src/components/ChatSpace.tsx
@@ -1,6 +1,56 @@
+import { ArrowUp } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { ScrollArea } from "./ui/scroll-area";
+
+const MessageReceived = () => {
+  return (
+    <div className="w-fit h-fit bg-[#F0F0F0] mr-auto p-3 rounded-xl">
+      Approved
+    </div>
+  )
+}
+
+const MessageSent = () => {
+  return (
+    <div className="w-fit h-fit bg-[#2546FF] ml-auto p-3 rounded-xl text-white">
+      Welcome on board
+    </div>
+  )
+}
+
 const ChatSpace = () => {
   return (
-    <div className="w-full bg-white h-full"></div>
+    <div className="w-full bg-white h-full flex flex-col-reverse pb-6 px-6">
+      <div className="relative w-full">
+        <Input className="rounded-xl" placeholder="Aa" />
+        <Button className="absolute right-1 top-1/2 -translate-y-1/2 w-8 h-8 rounded-full p-0 bg-[#0F75FF]">
+          <ArrowUp className="w-4" />
+        </Button>
+      </div>
+      <ScrollArea>
+        <div className="w-full h-full py-4 flex flex-col gap-4">
+          <MessageReceived />
+          <MessageSent />
+          <MessageReceived />
+          <MessageSent />
+          <MessageSent />
+          <MessageSent />
+          <MessageSent />
+          <MessageSent />
+          <MessageReceived />
+          <MessageSent />
+          <MessageReceived />
+          <MessageSent />
+          <MessageSent />
+          <MessageSent />
+          <MessageSent />
+          <MessageSent />
+          <MessageReceived />
+          <MessageReceived />
+        </div>
+      </ScrollArea>
+    </div>
   )
 }
 
